8882d6e7e257ec451475a9285472e58b5adb6a67
den13boec <den-boris@list.ru>
2025-10-03 21:10:07 +0300

    Add user_id for answer model

--- app/models/answer.py
+++ app/models/answer.py
@@ -1,16 +1,21 @@
+from uuid import uuid4
 from sqlalchemy import Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.base import Base
 
+
 class Answer(Base):
-    __tablename__ = 'answers'
+    __tablename__ = "answers"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     question_id: Mapped[int] = mapped_column(
-        ForeignKey('questions.id', ondelete='CASCADE'),
-        nullable=False
+        ForeignKey("questions.id", ondelete="CASCADE"), nullable=False
     )
-    user_id: Mapped[str] = mapped_column(String, nullable=False)
+    user_id: Mapped[str] = mapped_column(
+        String, nullable=False, default=lambda: str(uuid4())
+    )
     text: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    created_at: Mapped[str] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
 
-    question = relationship('Question', back_populates='answers')
+    question = relationship("Question", back_populates="answers")
--- app/models/question.py
+++ app/models/question.py
@@ -2,9 +2,17 @@ from sqlalchemy import Integer, String, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.base import Base
 
+
 class Question(Base):
-    __tablename__ = 'questions'
+    __tablename__ = "questions"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    answers = relationship('Answer', back_populates='question', cascade='all, delete-orphan', passive_deletes=True)
+    created_at: Mapped[str] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    answers = relationship(
+        "Answer",
+        back_populates="question",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+    )
