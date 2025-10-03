3ee050e3b6a34ff417b83f2067a98f92aea8bb93
den13boec <den-boris@list.ru>
2025-10-03 21:08:45 +0300

    Update for current db connection

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,15 @@
+from typing import Any, Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from app.core.config import settings
 
-engine = create_engine(settings.database_url, pool_pre_ping=True)
+engine = create_engine(settings.resolved_database_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db():
+
+def get_db() -> Generator[Session, Any, None]:
     db = SessionLocal()
     try:
         yield db
